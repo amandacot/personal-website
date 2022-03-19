667e01e99e68b067f2a3c8c43354542b3a705f6d
amandacot <67836328+amandacot@users.noreply.github.com>
2022-03-19 11:58:18 -0400

    completed hint and working on navbar color

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,22 +3,28 @@ if (currentTheme == "light") {
   $("body").addClass("light-theme");
   $(".moon").addClass('sun');
   $(".toggle-night").addClass('toggle-day');
+  $(".navbar-dark").addClass('navbar-light');
+  $(".bg-dark").addClass('bg-light');
 }
 $('.toggle-night').click(function() {
   $("body").toggleClass('light-theme');
   $(".moon").toggleClass('sun');
   $(".toggle-night").toggleClass('toggle-day');
+  $(".navbar-dark").toggleClass('navbar-light');
+  $(".bg-dark").toggleClass('bg-light');
+
   let theme = "dark";
-  const navbar = document.querySelector("nav");
-  const navbarClasses = navbar.classList;
+  // const navbar = document.querySelector("nav");
+  // const navbarClasses = navbar.classList;
   if ($("body").hasClass("light-theme")) {
     theme = "light";
-    navbar.classList.remove("navbar-dark", "bg-dark");
-    navbar.classList.add("navbar-light", "bg-light");
-  } else {
-    navbar.classList.remove("navbar-light", "bg-light");
-    navbar.classList.add("navbar-dark", "bg-dark");
+    // navbar.classList.remove("navbar-dark", "bg-dark");
+    // navbar.classList.add("navbar-light", "bg-light");
   }
+  // else {
+  //   navbar.classList.remove("navbar-light", "bg-light");
+  //   navbar.classList.add("navbar-dark", "bg-dark");
+  // }
   localStorage.setItem("theme", theme);
 
 });
